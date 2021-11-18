5faaff560ab81cdb95b7d81fd1dce9574246507d
olitskaya <94138631+olitskaya@users.noreply.github.com>
2021-11-18 15:31:26 +0200

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,5 +1,6 @@
-# Write a script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with tose numbers.
-sequence = input("Input a sequence of comma-separated numers: ")
+# Write a script which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+
+sequence = input("Input a sequence of comma-separated numbers: ")
 list = sequence.split(",")
 tuple = tuple(list)
 print('List: ', list)
